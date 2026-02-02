1b34853a4b0f64b154d2dda5362212e34394f8c8
Joshua Shim <joshua.shim2004@gmail.com>
2026-02-02 16:28:25 +1100

    working currently on remove the grail tree

---
+++ removeTheGrailTree.cpp
@@ -0,0 +1,24 @@
+// codeforces round 1076 (div. 3)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// 0 is even, 1 is odd,
+// priority queue based on ll.
+
+int main() {
+    // represent A and S as odd and evens, 
+    // keep removing even A's until there is none left
+    // How do i keep this? i think maybe a priority queue might be a good idea.
+    int t;
+    cin >> t;
+    for (long _i = 0; _i < t; ++_i) {
+        int n;
+        cin >> n;
+        vector<int> values(n);
+        for (long  i = 0; i < n; ++i) {
+            cin >> values[i];
+        }
+
+    }
+}
